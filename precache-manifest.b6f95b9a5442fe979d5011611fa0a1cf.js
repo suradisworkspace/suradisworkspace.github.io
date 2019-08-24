dc4eded9d020d0de3f4fb57f3d83fc6b079189aa
suradis sutampang <suradis.s@scale360solutions.com>
2019-08-25 03:00:54 +0700

    update to react

---
+++ precache-manifest.b6f95b9a5442fe979d5011611fa0a1cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80488319806efe596494a31d5162560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec66e2e2124debf9611b",
+    "url": "/static/css/main.6de3a644.chunk.css"
+  },
+  {
+    "revision": "ca47f26cd910e37a84a8",
+    "url": "/static/js/2.52c0e523.chunk.js"
+  },
+  {
+    "revision": "ec66e2e2124debf9611b",
+    "url": "/static/js/main.cecd4ebc.chunk.js"
+  },
+  {
+    "revision": "9d9941129469a6dbdef6",
+    "url": "/static/js/runtime~main.d640bfd6.js"
+  }
+]);
